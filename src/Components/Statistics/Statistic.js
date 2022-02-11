f0d229bd1842bdd9296266c88b1428abe9a8f770
Den4icGuti <goos7412@gmail.com>
2022-02-11 07:47:09 +0200

    add style

--- src/Components/Statistics/Statistic.js
+++ src/Components/Statistics/Statistic.js
@@ -1,14 +1,14 @@
 import styles from './Statistics.module.css';
-export default function Satistic(props) { 
-  const {label,percentage} =props
+export default function Satistic({label,percentage}) { 
+
   return (
     <section className={styles.statistics}>
       <h2 className={styles.title}>UPLOAD STATS</h2>
 
       <ul className={styles.statList}>
         <li className={styles.item}>
-      <span className={styles.label}>.docx</span>
-      <span className={styles.label}>4%</span>
+          <span className={styles.label}>{label}</span>
+          <span className={styles.label}>{percentage}</span>
     </li>
     <li className={styles.item}>
       <span className={styles.label}>.mp3</span>
