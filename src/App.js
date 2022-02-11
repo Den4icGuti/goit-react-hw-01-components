5c807f40bcb3f9c16bbefd4caf381be73bb488f2
Den4icGuti <goos7412@gmail.com>
2022-02-11 08:09:02 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Container from "Container/Container";
 import user from './Components/Profile/user.json';
 import Profile from './Components/Profile/Profile';
-import Statistic from './Components/Statistics/Statistic'
+ import Statistic from './Components/Statistics/Statistic'
 import stats from './Components/Statistics/data.json'
 function App() { 
   return (
@@ -13,7 +13,7 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistic stats={stats.data}/>
+      { <Statistic stats={stats.data}/> }
     </Container>
  )
 }
--- src/Components/Statistics/Statistic.js
+++ src/Components/Statistics/Statistic.js
@@ -1,4 +1,6 @@
 import styles from './Statistics.module.css';
+
+
 export default function Satistic({label,percentage}) { 
 
   return (
