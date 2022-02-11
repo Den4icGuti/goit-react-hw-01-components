93e6eb9c19c37fbeab280ed33c227000f3ced7f3
Den4icGuti <goos7412@gmail.com>
2022-02-11 14:59:01 +0200

    add card statistic

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistic stats={data} />
+      <Statistic item={data} />
+     
     </Container>
  )
 }
